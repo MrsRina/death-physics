a22218145c350b718c3eb36d99eb2c871edeb15c
Rina <37088203+MrsRina@users.noreply.github.com>
2023-12-15 18:20:40 -0300

    [build] unsense bugs

--- build/release/deps/physics_vk.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Diyng\Documents\GitHub\death-physics\./build\release\deps\physics_vk.exe: src\main.rs src\utility.rs src\utility\io.rs src\gpu.rs src\gpu\context.rs
-
-C:\Users\Diyng\Documents\GitHub\death-physics\./build\release\deps\physics_vk.d: src\main.rs src\utility.rs src\utility\io.rs src\gpu.rs src\gpu\context.rs
-
-src\main.rs:
-src\utility.rs:
-src\utility\io.rs:
-src\gpu.rs:
-src\gpu\context.rs:
--- build/release/deps/smallvec-40539ab043b34880.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Diyng\Documents\GitHub\death-physics\./build\release\deps\libsmallvec-40539ab043b34880.rmeta: C:\Users\Diyng\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.11.2\src\lib.rs
-
-C:\Users\Diyng\Documents\GitHub\death-physics\./build\release\deps\libsmallvec-40539ab043b34880.rlib: C:\Users\Diyng\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.11.2\src\lib.rs
-
-C:\Users\Diyng\Documents\GitHub\death-physics\./build\release\deps\smallvec-40539ab043b34880.d: C:\Users\Diyng\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.11.2\src\lib.rs
-
-C:\Users\Diyng\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.11.2\src\lib.rs:
